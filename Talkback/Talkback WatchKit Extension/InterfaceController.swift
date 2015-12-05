ccf5cb18deb2a26a8d609cd48fdbfeadc43a5c8f
Mark Goody <mgoody@shopkeep.com>
2015-12-04 22:09:42 -0500

    Add the buttons and connect them to the controller

--- Talkback/Talkback WatchKit Extension/InterfaceController.swift
+++ Talkback/Talkback WatchKit Extension/InterfaceController.swift
@@ -4,6 +4,7 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    
+    @IBOutlet var recordButton: WKInterfaceButton!
+    @IBOutlet var playButton: WKInterfaceButton!
 
 }
