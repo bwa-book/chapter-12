90c3cf110141bfb82e6a4ccd092092519d815e55
Mark Goody <mgoody@shopkeep.com>
2015-12-04 21:51:18 -0500

    Store playback position and resume playback from there

--- Media Savvy/Media Savvy WatchKit Extension/InterfaceController.swift
+++ Media Savvy/Media Savvy WatchKit Extension/InterfaceController.swift
@@ -6,11 +6,16 @@ class InterfaceController: WKInterfaceController {
 
     @IBOutlet var button: WKInterfaceButton!
     
+    var playbackPosition: NSTimeInterval = 0
+    
     @IBAction func play() {
         let bundle = NSBundle(forClass: InterfaceController.self)
         if let movieUrl = bundle.URLForResource("FirstSnow", withExtension: "m4v") {
-            presentMediaPlayerControllerWithURL(movieUrl, options: nil) { finished, endTime, error in
-                
+            let playerOptions = [
+                WKMediaPlayerControllerOptionsStartTimeKey: playbackPosition
+            ]
+            presentMediaPlayerControllerWithURL(movieUrl, options: playerOptions) { finished, endTime, error in
+                self.playbackPosition = finished ? 0 : endTime
             }
         }
     }
