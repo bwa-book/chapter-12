4b916c7b2f058dd9e04220c86a1ce8fd42a90265
Mark Goody <mgoody@shopkeep.com>
2015-12-04 22:10:54 -0500

    Manage play button state

--- Talkback/Talkback WatchKit Extension/InterfaceController.swift
+++ Talkback/Talkback WatchKit Extension/InterfaceController.swift
@@ -6,5 +6,16 @@ class InterfaceController: WKInterfaceController {
 
     @IBOutlet var recordButton: WKInterfaceButton!
     @IBOutlet var playButton: WKInterfaceButton!
+    
+    private var audioUrl: NSURL?
+    private func updatePlayButtonState() {
+        playButton.setEnabled(audioUrl != nil)
+    }
+    
+    override func willActivate() {
+        super.willActivate()
+        
+        updatePlayButtonState()
+    }
 
 }
