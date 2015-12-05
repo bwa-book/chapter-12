f32780e946d99032edd83a9becb0fc2e85e2854a
Mark Goody <mgoody@shopkeep.com>
2015-12-04 21:48:41 -0500

    Connect the button to play the movie

--- Media Savvy/Media Savvy WatchKit Extension/InterfaceController.swift
+++ Media Savvy/Media Savvy WatchKit Extension/InterfaceController.swift
@@ -5,5 +5,14 @@ import Foundation
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet var button: WKInterfaceButton!
+    
+    @IBAction func play() {
+        let bundle = NSBundle(forClass: InterfaceController.self)
+        if let movieUrl = bundle.URLForResource("FirstSnow", withExtension: "m4v") {
+            presentMediaPlayerControllerWithURL(movieUrl, options: nil) { finished, endTime, error in
+                
+            }
+        }
+    }
 
 }
