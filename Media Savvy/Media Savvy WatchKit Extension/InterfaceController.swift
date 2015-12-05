3b5f31076db0244bd90af1617ae3f3e17086230b
Mark Goody <mgoody@shopkeep.com>
2015-12-04 21:46:39 -0500

    Replace the movie player with a button

--- Media Savvy/Media Savvy WatchKit Extension/InterfaceController.swift
+++ Media Savvy/Media Savvy WatchKit Extension/InterfaceController.swift
@@ -4,21 +4,6 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    @IBOutlet var player: WKInterfaceMovie!
-    
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        var posterImage: WKImage?
-        if let image = UIImage(named: "poster") {
-            posterImage = WKImage(image: image)
-        }
-        player.setPosterImage(posterImage)
-        
-        let bundle = NSBundle(forClass: InterfaceController.self)
-        if let movieUrl = bundle.URLForResource("FirstSnow", withExtension: "m4v") {
-            player.setMovieURL(movieUrl)
-        }
-    }
+    @IBOutlet var button: WKInterfaceButton!
 
 }
